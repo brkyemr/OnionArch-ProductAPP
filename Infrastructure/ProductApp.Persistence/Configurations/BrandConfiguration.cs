578c8a35593b98234478615e3c1a504a7db6f408
brkyemr <berkayemr@gmail.com>
2024-03-12 02:54:41 +0300

    entity configurations added

---
+++ Infrastructure/ProductApp.Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductApp.Domain.Entities;
+
+namespace ProductApp.Persistence.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x=>x.Name).HasMaxLength(256);
+        }
+    }
+}
---
+++ Infrastructure/ProductApp.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApp.Persistence.Configurations
+{
+    public class CategoryConfiguration
+    {
+        
+    }
+}
