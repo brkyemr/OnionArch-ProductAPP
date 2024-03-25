e401690ab57e8eedb7056477237d9f7164c36d67
brkyemr <berkayemr@gmail.com>
2024-03-25 03:09:34 +0300

    Registration auto addTransient for entity base rules

--- Core/ProductApp.Application/Registration.cs
+++ Core/ProductApp.Application/Registration.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using ProductApp.Application.Bases;
 using ProductApp.Application.Behaviours;
 using ProductApp.Application.Exceptions;
 using ProductApp.Application.Features.Products.Rules;
@@ -20,12 +21,26 @@ namespace ProductApp.Application
             var assembly  = Assembly.GetExecutingAssembly();
 
             services.AddTransient<ExceptionMiddleware>();
-            services.AddTransient<ProductRules>();
+            //services.AddTransient<ProductRules>();
+            services.AddRulesFromAssemblyContaining(assembly,typeof(BaseRules));
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assembly));
             services.AddValidatorsFromAssembly(assembly);
             ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr");
 
             services.AddTransient(typeof(IPipelineBehavior<,>),typeof(FluentValidationBehavior<,>));
         }
+        private static IServiceCollection AddRulesFromAssemblyContaining(
+            this IServiceCollection services,
+            Assembly assembly,
+            Type type)
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+            {
+                services.AddTransient(item);
+            }
+            return services;
+        }
+
     }
 }
